842d9e09afb2c79b8229a1c6fff723f9e72a7a40
EnterVPL <woramowy@gmail.com>
2020-08-22 20:36:55 +0200

    Changed exe file to bat file

---
+++ README.md
@@ -0,0 +1,5 @@
+# CMD-UTF8
+## How to use
+* Clone BATCH file
+* Run on your CMD cmd2utf8.bat
+* Done
---
+++ cmd2utf8.bat
@@ -0,0 +1,9 @@
+@echo off
+echo I setting UTF-8 in your CMD.
+echo 'chcp 65001'
+chcp 65001
+echo Test:
+echo 'chcp'
+chcp
+PAUSE
+@echo on
